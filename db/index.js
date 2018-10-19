a7766b9fb96e2de2823ac2bb40e05301e1b8c974
JamesStark <31277977+JamesStark@users.noreply.github.com>
2018-10-19 13:08:19 +0530

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost/mahaperiyava");
+mongoose.connect("process.env.MONGODB_URI");
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
