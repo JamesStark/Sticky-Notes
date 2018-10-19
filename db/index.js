4538c3aa5862f4b760e6dbc110c4abe377a6ef84
Unknown <jamesstark145@gmail.com>
2018-10-19 13:13:08 +0530

    changes

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost/mahaperiyava");
+mongoose.connect("mongodb://stark:stark145@ds135653.mlab.com:35653/stickynotes");
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
